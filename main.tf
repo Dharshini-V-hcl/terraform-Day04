f816a17ba3ec0561acfea175f3b9c57e7c92f682
Dharshini-V-hcl <dharshini-v@hcltech.com>
2024-12-30 12:53:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_secretsmanager_secret" "db_password" {
+  name = "db-password"
+}
+
+data "aws_secretsmanager_secret_version" "db_password_version" {
+  secret_id = data.aws_secretsmanager_secret.db_password.id
+}
+
+output "db_password" {
+  value     = data.aws_secretsmanager_secret_version.db_password_version.secret_string
+  sensitive = true
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
+  instance_type = "t2.micro"
+
+  user_data = <<-EOT
+              #!/bin/bash
+              echo "DB_PASSWORD=${data.aws_secretsmanager_secret_version.db_password_version.secret_string}" > /etc/db_credentials
+              chmod 600 /etc/db_credentials
+              EOT
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
