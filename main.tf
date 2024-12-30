ea5c7c2bba1a7fd3e2e201467a68618272a33b67
Dharshini-V-hcl <dharshini-v@hcltech.com>
2024-12-30 22:50:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ output "db_password" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI ID for your region
+  ami           = "ami-01816d07b1128cd2d"
   instance_type = "t2.micro"
 
   user_data = <<-EOT
